c15570ba40965e243ab804115eaa3ef9c63d76e1
sturtuk <edwinsturt@hotmail.co.uk>
2018-07-17 19:11:43 +0530

    Check Payment

--- protected/components/Ajax.php
+++ protected/components/Ajax.php
@@ -3671,6 +3671,7 @@ $this->msg=t("We have sent bank information instruction to your email")." :$merc
 
 
         print_r($sale_info);
+        print_r($VApp);
         print_r($VApp->PaymentData($sale_info))->ProcessPayment();
         die();
 
