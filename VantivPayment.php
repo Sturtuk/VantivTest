772434b483b9bdfba52e176f4a3f6d052a2d3248
sturtuk <edwinsturt@hotmail.co.uk>
2018-07-17 19:38:48 +0530

    Check Payment

--- VantivPayment.php
+++ VantivPayment.php
@@ -13,31 +13,9 @@ class VantivPay
 
 	public function PaymentData(array $data){
 
-//        $initialize = new litle\sdk\LitleOnlineRequest();
-//        $this->saleResponse =$initialize->authorizationRequest($data);
-
-        $auth_info = array(
-            'orderId' => '1',
-            'amount' => '10010',
-            'id'=> '456',
-            'orderSource'=>'ecommerce',
-            'billToAddress'=>array(
-                'name' => 'John Smith',
-                'addressLine1' => '1 Main St.',
-                'city' => 'Burlington',
-                'state' => 'MA',
-                'zip' => '01803-3747',
-                'country' => 'US'
-            ),
-            'paypage'=>array(
-                'paypageRegistrationId' =>'4457010000000009',
-                'expDate' => '0112',
-                'cardValidationNum' => '349'
-            )
-        );
-
         $initialize = new litle\sdk\LitleOnlineRequest();
-        $this->saleResponse = $initialize->authorizationRequest($auth_info);
+        $this->saleResponse =$initialize->saleRequest($data);
+
 	            
 		return $this;
 
@@ -47,6 +25,7 @@ class VantivPay
 //				$response = (XmlParser::getNode($this->saleResponse,'response'));
 //				$message = XmlParser::getNode($this->saleResponse,'message');
 //				echo ("Vantiv eCommerce Transaction ID: " . XmlParser::getNode($this->saleResponse,'litleTxnId'));
+        return $this;
 	}
 
 }
