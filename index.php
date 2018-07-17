e9b51595a5ba6e7fd4f6867718b089e1b62645c9
sturtuk <edwinsturt@hotmail.co.uk>
2018-07-17 20:25:27 +0530

    Payment Done

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
 define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_ENABLE_EXCEPTION_HANDLER', false);
-ini_set("display_errors",false);
+ini_set("display_errors",true);
 //error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 // include Yii bootstrap file
